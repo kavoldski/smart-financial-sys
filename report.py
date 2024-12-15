93e25407d0e844d8a9c6a72847ed181722b9653c
kavoldski <kavoldski@gmail.com>
2024-12-15 21:31:21 +0800

    Delete report.py

--- report.py
+++
@@ -1,7 +0,0 @@
-# Class to create report for all user account
-class UserReport:
-    def __init__(self, user_list):
-        self.user_list = user_list
-        self.report = {}
-        self.create_report()
-        
